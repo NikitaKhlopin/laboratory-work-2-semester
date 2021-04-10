98293b8adace36c0be604fee8300d5ece315734d
NikitaKhlopin <74229659+NikitaKhlopin@users.noreply.github.com>
2021-04-10 18:29:59 +0500

    18.1
    18.1

---
+++ lab18_1/fraction.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include "fraction.h"
+
+using namespace std;
+
+void fraction::init(double f, double s) {
+	first = f;
+	second = s;
+}
+
+void fraction::read() {
+	cout << "first? "; cin >> first;
+	cout << "second? "; cin >> second;
+}
+
+void fraction::show() {
+	cout << "first = " << first << endl;
+	cout << "second = " << second << endl;
+}
+
+double fraction::root(double q) {
+	return (q-second)/first;
+}
