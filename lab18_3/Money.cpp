ee4a2eb80a3b3137e76e9b76ef4739bafdf9150e
NikitaKhlopin <74229659+NikitaKhlopin@users.noreply.github.com>
2021-04-11 20:13:24 +0500

    18.3
    18.3

---
+++ lab18_3/Money.cpp
@@ -0,0 +1,49 @@
+#include "money.h" 
+#include <iostream> 
+using namespace std;
+
+money& money::operator=(const money& t)
+{
+	if (&t == this)
+		return *this;
+	rub = t.rub;
+	pen = t.pen;
+	return *this;
+}
+
+money money::operator+(const money& t)
+{
+	long temp1 = rub * 100 + pen;
+	long temp2 = t.rub * 100 + t.pen;
+	money p;
+	p.rub = (temp1 + temp2) / 100;
+	p.pen = (temp1 + temp2) % 100;
+	return p;
+}
+
+money money::operator - (const money& t)
+{
+	long temp3 = rub * 100 - pen;
+	long temp4 = t.rub * 100 - t.pen;
+	money q;
+	q.rub = (temp4 - temp3) / 100;
+	q.pen = (temp4 - temp3) % 100;
+	return q;
+}
+
+istream& operator>>(istream& in, money& t)
+{
+	cout << "Введите рубли:";
+	cin >> t.rub;
+	cout << "Введите копейки:";
+	cin >> t.pen;
+	return in;
+}
+
+ostream& operator<<(ostream& out, const money& t)
+{
+
+	return (out << t.rub << "," << t.pen);
+}
+
+
